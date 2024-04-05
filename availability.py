562be9e1252d4c21eba568164cd3293f6d1778f0
lai <phuclaile2708@gmail.com>
2024-04-05 15:57:35 +0700

    update yaml

--- availability.py
+++ availability.py
@@ -10,15 +10,15 @@ config.load_kube_config()
 v1 = client.CoreV1Api()
 
 process = sys.argv[1]
-check_reaction = 0 #
-check_recover = 0
+check_reaction = 0 #check if reaction time has been recorded
+check_recover = 0 #check if recover time has been recorded
 
 
-start = time.time()
-os.system("sudo kill " + sys.argv[1])
+start = time.time() #Get current epoch time
+os.system("sudo kill " + sys.argv[1]) #kill process/container/pod
 
 while True:
-    # Read endpoints for the nginx-service in the default namespace
+    # Read endpoints for the nginx-service in the default namespace 
     ret = v1.read_namespaced_endpoints("nginx-service", "default", pretty=True)
 
     # Extract subsets from the result
@@ -27,6 +27,7 @@ while True:
     # Iterate through the subsets
     for subset in res:
         
+        # Check if whether service's IP address is listed in endpoints or not
         if subset.addresses is None and check_reaction == 0:
             reation_time = time.time() - start
             print("Reaction time: " + str(reation_time))
